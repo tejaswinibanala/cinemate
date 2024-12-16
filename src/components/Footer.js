a7d202e375b9127051d5ff6131c0b9e3b944ae9b
tejaswinibanala <tejaswinibanala28@gmail.com>
2024-12-16 12:27:37 -0600

    Updated Footer component and Netlify config

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ export const Footer = () => {
   return (
       <footer className="bg-white p-4 shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800">
 
-            <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 <Link to="https://flowbite.com/" className="hover:underline">Cinemate™</Link>. All Rights Reserved.
+            <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 <Link to="/" className="hover:underline">Cinemate™</Link>. All Rights Reserved.
             </span>
             <ul className="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
                 <li>
